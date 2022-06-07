897abb80994d583c9dfc35435c5c45cc56b0e137
Canberk <canberkkaya06@gmail.com>
2022-06-07 15:26:32 +0300

    added createNote function and create endpoint, added necessery iam permissions

---
+++ functions/createNote.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const dynamoDb = require('../config/dynamoDb')
+const { v4: uuidv4 } = require('uuid')
+
+//Creating response object
+function sendResponse(statusCode, body) {
+  const response = {
+    statusCode: statusCode,
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  }
+  return response
+}
+
+module.exports.createNote = async (event) => {
+  //this control is for testing from both serverless dasboard and postman
+  const body =
+    typeof event.body === 'string' ? JSON.parse(event.body) : event.body
+
+  try {
+    //collecting parameters
+    const { noteMessage } = body
+    const noteId = uuidv4()
+    const TableName = process.env.DYNAMODB_NOTE_TABLE
+    // defining params object
+    const params = {
+      TableName,
+      Item: {
+        noteId,
+        noteMessage,
+      },
+    }
+    //sending put request to dynamoDB
+    await dynamoDb.put(params).promise()
+    //success response
+    return sendResponse(200, {
+      message: 'Note successfully created',
+      item: params.Item,
+    })
+  } catch (error) {
+    console.log(error)
+    //error response
+    return sendResponse(500, { message: 'Could not create the note' })
+  }
+}
